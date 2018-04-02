8970ba3714c2637097be25fe23a90891c5989438
Pavlo <pavlo.shumylo@gmail.com>
2018-04-03 00:10:08 +0300

    Initial commit

---
+++ firstModule/src/main/java/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public void show() {
+        System.out.println("Hello first module from second module!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("First module");
+    }
+}
---
+++ secondModule/src/main/java/HelloSecond.java
@@ -0,0 +1,6 @@
+public class HelloSecond {
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+        hello.show();
+    }
+}
